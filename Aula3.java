ea52a89cfcdde3846c3116ac85ef268f95ac5861
willianscaquettunivem <124941070+willianscaquettunivem@users.noreply.github.com>
2023-02-10 01:29:02 +0000

    Aula3

---
+++ Aula3.java
@@ -0,0 +1,8 @@
+public class Aula3 {
+    public static void main(String[] args) {
+        float nota1 = 6.512f;
+        float nota2 = 4.2f;
+        System.out.println((nota1+nota2)/2); 
+      
+    }
+}
