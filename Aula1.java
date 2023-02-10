9906f358033800f330f84eedc26440f6eb4e28dd
willianscaquettunivem <124941070+willianscaquettunivem@users.noreply.github.com>
2023-02-10 01:00:32 +0000

    teste

---
+++ Aula1.java
@@ -0,0 +1,13 @@
+public class Aula1 {
+    public static void main(String[] args) {
+        int contador = 0;
+        while(contador < 100)   { 
+            System.out.println("bom dia"+ contador);
+            contador++;
+        }  
+    
+    
+
+    }
+
+}
